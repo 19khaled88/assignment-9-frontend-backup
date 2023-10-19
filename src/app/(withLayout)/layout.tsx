b4b226f3ce3aa86081d64db574c39d3570e0288a
19khaled88 <kkhaled88hasan@gmail.com>
2023-10-19 17:37:46 +0600

    updated content and sidebar props to funciton

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -8,20 +8,29 @@ import React from 'react';
 const { Header, Content, Footer, Sider } = Layout;
 
 
-const DashboardLayout = ({children}:{children:React.ReactNode}) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
- 
+
   return (
     <Layout style={{ minHeight: '100vh' }} hasSider>
       {/* <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
         <div className="demo-logo-vertical" />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider> */}
-      <SidebarPage/>
+
+      {/* <SidebarPage /> */}
+      {
+        SidebarPage()
+      }
+
       {/* <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }} /> */}
-        <ContentPage children={children}/>
-        {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+
+      {/* <ContentPage children={children}/> */}
+      {
+        ContentPage({ children })
+      }
+      {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
       </Layout> */}
     </Layout>
   );
