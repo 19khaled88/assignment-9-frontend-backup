41e9e7270ba7c5a3c0068e81bcba23cb4ad2e99c
19khaled88 <kkhaled88hasan@gmail.com>
2023-10-19 22:25:23 +0600

    changed base url

--- src/helpers/config/publicUrl.ts
+++ src/helpers/config/publicUrl.ts
@@ -1,3 +1,4 @@
 export const getBaseUrl = (): string => {
-    return process.env.BACKEND_PUBLIC_URL || "http://localhost:3000/api/v1"
+    return process.env.BACKEND_PUBLIC_URL || "http://localhost:3039/api/v1"
+    
 }
