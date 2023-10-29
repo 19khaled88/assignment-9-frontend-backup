f1779ad5128a81a59db5ba8ceb0649345894a795
19khaled88 <kkhaled88hasan@gmail.com>
2023-10-29 11:38:20 +0600

    animate navbar

--- src/app/(siteLayout)/layout.tsx
+++ src/app/(siteLayout)/layout.tsx
@@ -1,60 +1,27 @@
-'use client'
+
 import Navbar from "@/components/navbar/navbar";
 import { Footer } from "@/components/ui/Footer";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from "react";
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+
 export default function SiteLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode
 }) {
 
- // const ref = useRef(null)
-    // const isInView = useInView(ref)
-    // useEffect(() => {
-    //     AOS.init({
-    //         delay:30,
-    //         duration:400,
-    //         easing:'ease-out',
-    //         once:false,
-    //         mirror:true,
-    //         offset:120,
-    //         debounceDelay:50,
-    //         throttleDelay:99
-    //     });
-    //     AOS.refresh();
-    //   }, [isInView]);
-
-  //  AOS && AOS.init({
-  //     // duration: 1200,
-  //     disable: false,
-  //     startEvent: 'load',
-  //     initClassName: 'aos-init',
-  //     animatedClassName: 'aos-animate',
-  //     useClassNames: false,
-  //     disableMutationObserver: false,
-  //     debounceDelay: 50,
-  //     throttleDelay: 99,
-
-  //     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  //     offset: 120,
-  //     delay: 0,
-  //     duration: 400,
-  //     easing: 'ease-in-out-quad',
-  //     once: false,
-  //     mirror: false,
-  //     anchorPlacement: 'top-bottom'
-  //   })
-
-
 
   return (
+    
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <Navbar />
+      <Navbar 
+      
+      />
       <ToastContainer />
       {children}
       <Footer />
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,21 +1,53 @@
 'use client'
+import { motion, useScroll, useVelocity } from 'framer-motion';
 import Link from "next/link";
-import React, { useState} from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineHome } from 'react-icons/ai';
 
+import Image from 'next/image';
 import { BsCart4 } from 'react-icons/bs';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { MdOutlineFeaturedPlayList } from 'react-icons/md';
 import { TbListDetails } from 'react-icons/tb';
 import { VscAccount } from 'react-icons/vsc';
 import styles from '../../app/styles/nav.module.css';
-import Image from 'next/image'
 const logo = '/logo.jpg'
-
+const slideDistance = 100
+const threshold = 500
 const Navbar = () => {
     const [nav, setNav] = useState(false);
-  
-    
+    const { scrollY } = useScroll()
+    const scrollVelocity = useVelocity(scrollY)
+
+    const [isScrollingBack, setIsScrollingBack] = useState(false)
+    const [isAtTop, setIsAtTop] = useState(true)
+    const [isInView, setIsInView] = useState(true)
+
+
+    useEffect(() => {
+        scrollVelocity.onChange((latest) => {
+            if (latest > 0) {
+                setIsScrollingBack(false)
+                return
+            }
+            if (latest < -threshold) {
+                setIsScrollingBack(true)
+                return
+            }
+        })
+    }, [])
+
+    useEffect(
+        () => scrollY.onChange((latest) => setIsAtTop(latest <= 0)),
+        []
+    )
+
+    useEffect(
+        () => setIsInView(isScrollingBack || isAtTop),
+        [isScrollingBack, isAtTop]
+    )
+
+
 
     const links = [
         {
@@ -49,11 +81,17 @@ const Navbar = () => {
             icon: <VscAccount size={15} />
         },
     ];
-    
+
     return (
-        <div 
-        className="" 
-        style={{ display: 'flex', justifyContent: 'space-between', height: '70px', alignItems: 'center', width: '100%', background: '#3876BF', color: 'white', position: 'relative' }}
+        <motion.div
+            className={`flex justify-between items-center w-full text-white h-16  z-50  ${isInView === true ? 'relative bg-teal-500' : 'fixed bg-blue-400'} `}
+        // className={color ? 'z-20 top-0 fixed w-screen transition-all duration-300 text-white' : 'z-20 top-0 fixed w-screen transition-all duration-300 bg-grayOne'}
+        // style={{ display: 'flex', justifyContent: 'space-between', height: '70px', alignItems: 'center', width: '100%', background: '#3876BF', color: 'white', position: 'fixed',zIndex:'99' }}
+
+
+        // animate={{ y: isInView ? 0 : -slideDistance }}
+        // transition={{ duration: 0.2, delay: 0.25, ease: 'easeInOut' }}
+
         >
 
             <div style={{ paddingLeft: '20px' }}>
@@ -67,7 +105,7 @@ const Navbar = () => {
                     >
                         Logo
                     </a> */}
-                    <Image src={logo} alt="No Logo" width={50} height={50} className="rounded-md"/>
+                    <Image src={logo} alt="No Logo" width={50} height={50} className="rounded-md" />
                 </h1>
             </div>
             <div className={styles.clickIcon} style={{ paddingRight: '20px' }}>
@@ -118,8 +156,7 @@ const Navbar = () => {
             </div>
 
 
-        </div>
-
+        </motion.div>
     );
 };
 
