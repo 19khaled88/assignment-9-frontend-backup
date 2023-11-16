2935f109988a1f90e5c1864407de7b65621b5bc7
19khaled88 <kkhaled88hasan@gmail.com>
2023-11-16 16:49:34 +0600

    switch to public

--- src/helpers/config/publicUrl.ts
+++ src/helpers/config/publicUrl.ts
@@ -1,7 +1,7 @@
-const pub_url = 'https://assignment-9-backend.vercel.app/api/v1'
+// const pub_url = 'https://assignment-9-backend.vercel.app/api/v1'
 // const pub_url = 'https://assignment-9-backend.vercel.app/api/v1/'
 export const getBaseUrl = (): string => {
     // return process.env.BACKEND_PUBLIC_URL || "http://localhost:3039/api/v1"
-    return pub_url || "http://localhost:3039/api/v1"
-    // return "http://localhost:3039/api/v1"
+    // return pub_url || "http://localhost:3039/api/v1"
+    return "http://localhost:3039/api/v1"
 }
