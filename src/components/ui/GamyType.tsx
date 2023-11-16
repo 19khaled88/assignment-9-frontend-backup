1e78758e305fa8a6d1f0cd66753f96d09c192acb
19khaled88 <kkhaled88hasan@gmail.com>
2023-11-16 23:38:51 +0600

    image url

--- src/components/ui/GamyType.tsx
+++ src/components/ui/GamyType.tsx
@@ -58,7 +58,7 @@ const GameTypePage = () => {
             array.push(
 
                 <div className={`${styles.hoverItem} card w-full bg-base-100 shadow-xl`}>
-                    <img className="rounded-t-lg" src={item.url} alt="No game types" />
+                    <img className="rounded-t-lg" src={item.imgurl} alt="No game types" />
                     <div className="card-body p-4">
                         <h2 className="card-title">
 
